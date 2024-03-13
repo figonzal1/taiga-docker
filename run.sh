b43b420e1a9e97409a9fef127d74c9b29ade9bad
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-03-13 09:56:21 -0300

    fix path & kubectl args
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- run.sh
+++ run.sh
@@ -5,46 +5,46 @@ microk8s kubectl create namespace taiga
 microk8s kubectl create configmap taiga-config --from-env-file=.env -n taiga
 
 #DB
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-db-data-persistentvolumeclaim.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-db-deployment.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-db-service.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-db-data-persistentvolumeclaim.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-db-deployment.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-db-service.yaml
 
 # RABBIT MQ EVENTS
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-events-rabbitmq-data-persistentvolumeclaim.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-events-rabbitmq-deployment.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-events-rabbitmq-service.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-events-rabbitmq-data-persistentvolumeclaim.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-events-rabbitmq-deployment.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-events-rabbitmq-service.yaml
 
 # Taiga Events
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-events-deployment.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-events-service.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-events-deployment.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-events-service.yaml
 
 # Taiga Protected
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-protected-deployment.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-protected-service.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-protected-deployment.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-protected-service.yaml
 
 # RABBIT MQ ASYNC
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-async-rabbitmq-data-persistentvolumeclaim.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-async-rabbitmq-deployment.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-async-rabbitmq-service.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-async-rabbitmq-data-persistentvolumeclaim.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-async-rabbitmq-deployment.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-async-rabbitmq-service.yaml
 
 # Static & Media Data
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-static-data-persistentvolumeclaim.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-media-data-persistentvolumeclaim.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-static-data-persistentvolumeclaim.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-media-data-persistentvolumeclaim.yaml
 
 # Taiga ASYNC
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-async-deployment.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-async-service.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-async-deployment.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-async-service.yaml
 
 # Taiga BACK
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-back-deployment.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-back-service.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-back-deployment.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-back-service.yaml
 
 # Taiga Front
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-front-deployment.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-front-service.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-front-deployment.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-front-service.yaml
 
 # Taiga Gateway
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-gateway-configmap.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-gateway-deployment.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-gateway-service.yaml
-microk8s kubectl apply -f -n taiga /taiga-k8s/taiga-gateway-ingress.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-gateway-configmap.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-gateway-deployment.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-gateway-service.yaml
+microk8s kubectl apply -n taiga -f ./taiga-k8s/taiga-gateway-ingress.yaml
